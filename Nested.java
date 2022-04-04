b9bc2097fb69a3109ac9380ab097a72aeed16991
benoirajan <benoirajan@gmail.com>
2022-04-04 14:18:08 +0530

    Matrix

--- Nested.java
+++ Nested.java
@@ -2,6 +2,7 @@ public class Nested{
 	public static void main(String[] ar){
 		new Parent().new Child().display();
 		new Parent().print("HI");
+		new Parent().inFunCls();
 	}
 }
 
@@ -13,5 +14,17 @@ class Parent{
 		void display(){
 			print("This is Child function");
 		}
+	}
+
+/**
+ *It used for test class in a function 
+ */
+	public void inFunCls(){
+		class InFunCls{
+			public void test(){
+				System.out.println("InFunCls");
+			}
+		}
+		new InFunCls().test();
 	}
 }
