1752a9163772f245fb34925646af0be698be4509
benoirajan <benoirajan@gmail.com>
2022-05-18 15:44:45 +0530

    sd

--- DigitSum.java
+++ DigitSum.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class DigitSum{
 	public static void main(String[] args) {
-		int n = 431;
+		System.out.print("Enter a no.: ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println("Sum of digits is: "+sum(n));
+	}
+
+	public static int sum(int n){
 		int s = 0;
 		while(n>0){
 			s+=n%10;
 			n/=10;
 		}
-		System.out.println("Sum of digits is: "+s);
+		return s>9?sum(s):s;
 	}
 }
+
+/*
+Enter a no.: 33
+Sum of digits is: 6
+*/
